31d3b38b177249d522b20e789678380e588a048c
Mohammad Al Khano <Mu92.sm11@gmail.com>
2021-08-30 16:03:36 +0200

    Added UserControll() method to Program

--- PrimeNumberCalculator/Program.cs
+++ PrimeNumberCalculator/Program.cs
@@ -6,15 +6,32 @@ namespace PrimeNumberCalculator
     {
         static void Main(string[] args)
         {
-            while (true)
+            Program.UserControll();
+        }
+        static void UserControll()
+        {
+            bool wontsToCalculate = true;
+            while (wontsToCalculate)
             {
                 Console.Write("Please input a number to check if it a prime number: ");
                 string userInput = Console.ReadLine();
                 PrimeNumber.CheckInput(userInput);
+                Console.WriteLine("Do you wont to calculate new number?\n" + "Press[1] Yes\nPress[2] No");
+                int input = int.Parse(Console.ReadLine());
+                if (input==1)
+                {
+                    wontsToCalculate = true;
+                }
+                else if(input==2)
+                {
+                    wontsToCalculate = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please choose [1] or [2]");
+                }
 
             }
-
-            Console.Read();
         }
     }
 }

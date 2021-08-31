b05c39aaecd716bf57089a4d0a736d4500c0fb9f
Mohammad Al Khano <Mu92.sm11@gmail.com>
2021-08-31 22:55:57 +0200

    Added new class PrimeNumberControll

--- PrimeNumberCalculator/Program.cs
+++ PrimeNumberCalculator/Program.cs
@@ -6,72 +6,7 @@ namespace PrimeNumberCalculator
     {
         static void Main(string[] args)
         {
-
-            Program.StartControll();           
-            
+            PrimeNumberControll.StartUp();
         }
-        /// <summary>
-        /// Start up method to controll and communicate with the user and the program  .
-        /// </summary>
-        private static void StartControll()
-        {
-
-            bool wantToCalculate = true;
-            while (wantToCalculate)
-            {
-                Console.Clear();
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("\n\t\t\t-------------------- Prime Number Calculator --------------------");
-                Console.Write("\nPlease input a number to check if it a prime number: ");
-
-                string userInput = Console.ReadLine();
-                PrimeNumber.CheckInput(userInput);
-            choices:
-                try
-                {
-
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    Console.WriteLine("\n\nDo you wont to calculate new number?\n Press [1] Yes\n Press [2] No \n Press [3] if you wont to print all inputed Prime Numbers\n Press [4] To check next prime number");
-                    int input = int.Parse(Console.ReadLine());
-                    if (input == 1)
-                    {
-                        wantToCalculate = true;
-                    }
-                    else if (input == 2)
-                    {
-                        wantToCalculate = false;
-                        break;
-                    }
-                    else if (input == 3)
-                    {
-                        PrimeNumber.Print();
-                        goto choices;
-                    }
-                    else if (input == 4)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkGreen;
-                        var nextNumber = PrimeNumber.CheckNextPrime();
-                        Console.WriteLine($"The next prime number is {nextNumber}");
-                        goto choices;
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.WriteLine("Invalid choose!, You have four choices [1],[2],[3] or [4]");
-                        goto choices;
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("Invalid choose!, You have four choices [1],[2],[3] or [4]");
-                    goto choices;
-
-                }
-
-
-            }
-        }
-               
     }
 }
